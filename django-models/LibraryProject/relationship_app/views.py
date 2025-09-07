b21f9fb301df9c5177d949ecf409daf425861580
Brenda <brendagovha1@gmail.com>
2025-09-07 20:47:50 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -59,7 +59,7 @@ def logout_view(request):
     return render(request, 'relationship_app/logout.html')
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('relationship_app:login')
     
