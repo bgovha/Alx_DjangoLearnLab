27181c50f26ba81121ecb462beb1bf0df4db9ce6
Brenda <brendagovha1@gmail.com>
2025-09-07 10:47:53 +0200

    Create urls.py

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'relationship_app'
+
+urlpatterns = [
+    # Function-based view URL
+    path('books/', views.list_books, name='list_books'),
+    
+    # Class-based view URL
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
