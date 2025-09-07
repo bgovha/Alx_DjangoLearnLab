bf317c61ba65641e635e2da619f9a77ec9c4dfbf
Brenda <brendagovha1@gmail.com>
2025-09-07 11:39:17 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib import admin
+from django.urls import path, include
 
 app_name = 'relationship_app'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relationship/', include('relationship_app.urls')),
     # Function-based view URL
     path('books/', views.list_books, name='list_books'),
     
