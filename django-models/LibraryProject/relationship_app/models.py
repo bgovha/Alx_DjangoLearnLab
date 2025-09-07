766de774cb5a138b4ecda4e5fb906f5675d9288c
Brenda <brendagovha1@gmail.com>
2025-09-07 21:57:39 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -16,6 +16,14 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+            ("can_view_book", "Can view book"),
+        ]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
