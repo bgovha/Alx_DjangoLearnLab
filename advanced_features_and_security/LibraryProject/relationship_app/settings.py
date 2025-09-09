ca032dc3c646b39669c24ffd00addde8e5aca3db
Brenda <brendagovha1@gmail.com>
2025-09-09 14:06:27 +0200

    Update settings.py

--- advanced_features_and_security/LibraryProject/relationship_app/settings.py
+++ advanced_features_and_security/LibraryProject/relationship_app/settings.py
@@ -1,7 +1,16 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Add to the existing settings
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'relationship_app/static'),
 ]
+
+# Add to your existing settings
+AUTH_USER_MODEL = 'relationship_app.CustomUser'  # Replace 'relationship_app' with your actual app name
+
+# Add media settings for profile photos
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
