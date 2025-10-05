972579aa12065066b3f7c10cb9b74c8390b929cf
Brenda Govha <151570373+bgovha@users.noreply.github.com>
2025-10-05 00:46:06 +0000

    Commit all recent changes including migrations, models, views, forms, URLs, and templates for django_blog project.

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Post, Comment, Tag
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'published_date', 'view_count', 'created_date')
+    list_filter = ('status', 'published_date', 'created_date', 'tags')
+    search_fields = ('title', 'content', 'excerpt')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_date'
+    ordering = ('-published_date',)
+    raw_id_fields = ('author',)
+    filter_horizontal = ('tags',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_date', 'approved')
+    list_filter = ('approved', 'created_date')
+    search_fields = ('author__username', 'content', 'post__title')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "Approve selected comments"
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_date')
+    search_fields = ('name',)
--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,3 +1,78 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Post, Tag
+
+class BlogCRUDTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpass123'
+        )
+        self.other_user = User.objects.create_user(
+            username='otheruser',
+            email='other@example.com',
+            password='otherpass123'
+        )
+        self.tag = Tag.objects.create(name='Django')
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='This is a test post content.',
+            author=self.user,
+            status='published'
+        )
+        self.post.tags.add(self.tag)
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('blog:post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Post')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get(reverse('blog:post_detail', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Post')
+        self.assertTemplateUsed(response, 'blog/post_detail.html')
+
+    def test_post_create_view_authenticated(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(reverse('blog:post_create'))
+        self.assertEqual(response.status_code, 200)
+        
+        # Test POST request
+        response = self.client.post(reverse('blog:post_create'), {
+            'title': 'New Test Post',
+            'content': 'This is a new test post content.',
+            'status': 'published',
+            'tags': [self.tag.pk]
+        })
+        self.assertEqual(response.status_code, 302)  # Redirect after success
+        self.assertTrue(Post.objects.filter(title='New Test Post').exists())
+
+    def test_post_create_view_unauthenticated(self):
+        response = self.client.get(reverse('blog:post_create'))
+        self.assertEqual(response.status_code, 302)  # Redirect to login
+
+    def test_post_update_view_author(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(reverse('blog:post_update', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_update_view_non_author(self):
+        self.client.login(username='otheruser', password='otherpass123')
+        response = self.client.get(reverse('blog:post_update', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 403)  # Forbidden
+
+    def test_post_delete_view(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(reverse('blog:post_delete', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)
+        
+        # Test POST request for deletion
+        response = self.client.post(reverse('blog:post_delete', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 302)  # Redirect after success
+        self.assertFalse(Post.objects.filter(pk=self.post.pk).exists())
