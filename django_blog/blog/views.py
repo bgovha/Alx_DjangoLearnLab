a7716ea6e133e5fa658b85b58a9b66abcbc405cb
Brenda Govha <151570373+bgovha@users.noreply.github.com>
2025-10-05 00:34:54 +0000

    Inject advanced blog views and fix missing User import for user post listing and context.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,63 +1,180 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post, Comment
-from .forms import CommentForm
-from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView, View
+)
+from django.urls import reverse_lazy
+from django.db.models import Q, Count
+from django.utils import timezone
+from .models import Post, Comment, Tag
+from django.contrib.auth.models import User
+from .forms import PostForm, CommentForm, TagForm
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-published_date']
-    paginate_by = 5
+    paginate_by = 6
+    
+    def get_queryset(self):
+        queryset = Post.objects.filter(
+            status='published', 
+            published_date__lte=timezone.now()
+        ).select_related('author').prefetch_related('tags')
+        
+        # Filter by tag if provided
+        tag_slug = self.request.GET.get('tag')
+        if tag_slug:
+            queryset = queryset.filter(tags__name=tag_slug)
+        
+        # Search functionality
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(excerpt__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+        
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['popular_tags'] = Tag.objects.annotate(
+            post_count=Count('posts')
+        ).order_by('-post_count')[:10]
+        context['search_query'] = self.request.GET.get('q', '')
+        return context
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    
+    def get_queryset(self):
+        return Post.objects.select_related('author').prefetch_related('tags', 'comments__author')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comment_form'] = CommentForm()
+        context['similar_posts'] = self.get_similar_posts()
         return context
-
-    # Add to blog/views.py PostDetailView
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = self.object
-            comment.author = request.user
-            comment.save()
-            return redirect('blog:post_detail', pk=self.object.pk)
-        return self.get(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        # Increment view count
+        if self.object.is_published:
+            self.object.increment_view_count()
+        return response
+    
+    def get_similar_posts(self):
+        post_tags_ids = self.object.tags.values_list('id', flat=True)
+        similar_posts = Post.objects.filter(
+            tags__in=post_tags_ids,
+            status='published',
+            published_date__lte=timezone.now()
+        ).exclude(id=self.object.id)
+        similar_posts = similar_posts.annotate(
+            same_tags=Count('tags')
+        ).order_by('-same_tags', '-published_date')[:3]
+        return similar_posts
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content', 'tags', 'image']
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
     
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Your post has been created successfully!')
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create New Post'
+        context['submit_text'] = 'Create Post'
+        return context
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content', 'tags', 'image']
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Your post has been updated successfully!')
+        return super().form_valid(form)
     
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update Post'
+        context['submit_text'] = 'Update Post'
+        return context
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = '/'
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
     
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Your post has been deleted successfully!')
+        return super().delete(request, *args, **kwargs)
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 6
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(
+            author=user,
+            status='published',
+            published_date__lte=timezone.now()
+        ).select_related('author').prefetch_related('tags')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile_user'] = get_object_or_404(User, username=self.kwargs.get('username'))
+        return context
+
+@login_required
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            messages.success(request, 'Your comment has been added!')
+            return redirect('blog:post_detail', pk=post.pk)
+    
+    return redirect('blog:post_detail', pk=post.pk)
+
+@login_required
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.user == comment.author or request.user == comment.post.author:
+        post_pk = comment.post.pk
+        comment.delete()
+        messages.success(request, 'Comment deleted successfully!')
+        return redirect('blog:post_detail', pk=post_pk)
+    else:
+        messages.error(request, 'You are not authorized to delete this comment.')
+        return redirect('blog:post_detail', pk=comment.post.pk)
 
 def search_posts(request):
     query = request.GET.get('q')
